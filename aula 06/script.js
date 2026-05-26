72bfe99a6cbe76a5ebf607db73c3d851abba7922
Luiz Fressato <luizfressat@gmail.com>
2026-05-26 09:14:53 -0300

    aula 6

---
+++ aula 06/script.js
@@ -0,0 +1,43 @@
+let fruta = "Maça"
+let fruta2= "Uva"
+let fruta3 = "Pêra"
+
+console.log(fruta)
+console.log(fruta2)
+console.log(fruta3)
+
+let frutas = ["Maça", "Uva", "Pêra"]
+console.log(frutas)
+
+let pessoa = {
+    nome: "João",
+    idade: 28,
+    profissao: "Engenheiro"
+};
+console.log(pessoa)
+
+let alunos = [
+    {
+        id:1,
+        nome: "Murilo",
+        sobrenome: "lima"
+    },
+    {
+        id:2,
+        nome: "Vicius",
+        sobrenome: "lima"
+    },
+    {
+        id:3,
+        nome: "Davi",
+        sobrenome: "lima"
+    }
+]
+console.log(alunos[2])
+
+
+function soma(n1, n2){
+     return n1+ n2
+}
+console.log(soma(10,20))
+console.log(soma(50,30))
