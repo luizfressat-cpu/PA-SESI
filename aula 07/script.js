719f3b501130269217eb3f628b009fa5d0307f7a
Luiz <luizfressat@gmail.com>
2026-06-02 09:16:03 -0300

    aula 7

---
+++ aula 07/script.js
@@ -0,0 +1,33 @@
+
+function exibirNome(){
+    let nome = document.querySelector('#nome'). value
+    alert(nome)
+}
+
+
+function somar(){
+    let valA = Number (document.querySelector('#valorA'). value)
+    let valB = Number (document.querySelector('#valorB'). value)
+    alert(valA + valB + valC + valD)
+}
+
+function multiplicar(){
+    let valO = Number (document.querySelector('#valorO'). value)
+    let valZ = Number (document.querySelector('#valorZ'). value)
+    alert(valO * valZ)
+}
+
+
+function subtração(){
+    let valM = Number (document.querySelector('#valorM'). value)
+    let valL = Number (document.querySelector('#valorL'). value)
+    alert(valM - valL)
+}
+
+
+function Divisão(){
+    let valK = Number (document.querySelector('#valorK'). value)
+    let valF = Number (document.querySelector('#valorF'). value)
+    alert(valK / valF)
+}
+
